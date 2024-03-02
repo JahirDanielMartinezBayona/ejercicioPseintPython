2b83f649111cb453b61712857c7b262f9a41b70f
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-03-01 20:26:33 -0500

    Promedio

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-radio = input("Ingrese el radio: ")
-area = math.pi * float(radio) * float(2)
-perimetro = math.pi * float(radio) ** 2
-print("Perímetro: " + str(perimetro))
-print("Área: " + str(area))
+primeraNota = input("Primera nota: ")
+segundaNota = input("Segundo nota: ")
+terceraNota = input("Tercera nota: ")
+cuartaNota = input("Cuarta nota: ")
+promedio= (float(primeraNota)+float(segundaNota)+float(terceraNota)+float(cuartaNota))/4
+print("El promedio es: "+ str(promedio))
