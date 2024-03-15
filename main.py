c592fbc6c53c5a1ad06f0cae4b55883c6ffb889b
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-03-15 06:41:01 -0500

    Parte decimal

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-print(" PROGRAMA PARA CONOCER SI EL NÚMERO ES PAR O IMPAR")
-numeroParImpar = int(input("Ingresa tu número: "))
-if numeroParImpar % 2 == 0:
-    print(" Su número es par ")
-else:
-    print(" Su número es impar ")
+print("""
+PROGRAMA QUE ENTREGA LA PARTE DECIMAL DE UN NÚMERO REAL INGRESADO POR EL USUARIO
+      """)
+numero = float(input("Ingresa tu número: "))
+if(numero > 0): 
+      numeroDecimal = float(numero - int(numero))      
+else: 
+      numeroDecimal = float(int(numero) - numero)
+
+print("La parte decimal es: "+str(numeroDecimal))
