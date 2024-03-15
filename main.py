62954d72c6ad5dae12c07c6f3cf255b6df19cb70
JahirDanielMartinezBayona <160861323+JahirDanielMartinezBayona@users.noreply.github.com>
2024-03-15 13:14:34 -0500

    Años bisiestos

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 print("""
-PROGRAMA QUE PREGUNTA AL USUARIO LAS NOTAS 
-DE LOS DOS PRIMEROS CERTAMEN Y LA NOTA DE LABORATORIO, 
-Y MUESTRA LA NOTA QUE NECESITA EL ALUMNO PARA APROBAR 
-EL RAMO CON NOTA FINAL 60.
+PROGRAMA QUE INDIQUE SI UN AÑO ES BISIESTO O NO, 
+TENIENDO EN CUENTA CUÁL ERA EL CALENDARIO VIGENTE EN ESE AÑO
       """)
-notaCertamen1 = float(input("Ingrese nota certamen 1: "))
-notaCertamen2 = float(input("Ingrese nota certamen 2: "))
-notaLabotatorio = float(input("Ingrese nota laboratorio: "))
-
-
-notaCertamen3 = ((60 - 0.3*notaLabotatorio)/0.7)*3 - notaCertamen1 - notaCertamen2
-if notaCertamen3 > 0:
-      print("Necesita nota "+str(notaCertamen3)+" en el certamen 3") 
-
+anio = int(input("Ingrese un año: "))
+anioBisiesto = anio % 4
+if anioBisiesto == 0:
+      print(str(anio)+" es bisiesto")
 else:
-      print("No necesitas nota alta en el certamen 3") 
+      print(str(anio)+" no es bisiesto")
